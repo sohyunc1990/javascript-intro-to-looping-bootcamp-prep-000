7d1a30843020af2e9b9885927a0f08fe27e0187f
Sohyun Lee <sohyunc1990@gmail.com>
2020-06-20 18:41:03 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,30 +1,28 @@
-function forLoop(array) {
-   for ( let i = 0; i < 25; i++) {
-     if (i === 1) {
-       array.push(`I am 1 strange loop.`)
-     } else {
-       array.push(`I am ${i} strange loops.`)
-     }
+function forLoop(array){
+  for (let i = 0; i < 25; i++) {
+  if (i === 1) {
+
+    array.push(`I am 1 strange loop.`)
+  } else {
+
+    array.push(`I am ${i} strange loops.`)
   }
+}
   return array;
 }
 
-function whileLoop(n) {
-  let countdown = n;
+function whileLoop(n){
+  while (n > 0){
+    console.log(-- n);
+  }if(n === 0)
+  return "done";
+}
 
-    while (countdown > 0) {
-      console.log(--countdown)
-    }
-    return "done";
-  }
+var num = 0;
 
-function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
-
-  do {
-    array.pop();
-  } while (array.length > -1 && maybeTrue());
-    return array;
+function doWhileLoop(num) {
+  var i = 1;
+  do{
+    console.log("I run once regardless.");
+  }while(i++ < num);
 }
